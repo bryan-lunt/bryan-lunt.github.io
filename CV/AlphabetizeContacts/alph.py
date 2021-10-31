b485cb61c5a1a2e7e97342ba92a5a40438076676
Bryan Lunt <bjlunt2@illinois.edu>
2021-10-31 16:55:21 -0500

    working from full NSF CV.

---
+++ CV/AlphabetizeContacts/alph.py
@@ -0,0 +1,8 @@
+names = [
+		'Szurmant', 'Procaccini', 'Hoch', 'Hwa', 'Weigt', 'Morcos', 'Pagnani', 'Bertolino', 'Marks', 'Sander', 'Zecchina', 'Onuchic'
+		]
+
+names.sort()
+
+for i in names:
+	print i
